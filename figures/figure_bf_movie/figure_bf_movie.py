93232114c893aa14b7229e8101e73998240dd1e9
Niall Murphy <nmurphy@gmail.com>
2018-05-03 14:29:24 +0100

    make same width

--- figures/figure_bf_movie/figure_bf_movie.py
+++ figures/figure_bf_movie/figure_bf_movie.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import numpy as np
+#import numpy as np
 import os.path
 import skimage.io
 import matplotlib.pyplot as plt
@@ -13,10 +13,17 @@ this_dir = os.path.dirname(__file__)
 stylefile = os.path.join(this_dir, '../figstyle.mpl')
 plt.style.use(stylefile)
 import lib.figure_util as figure_util
-from lib.figure_util import dpi, strain_color, strain_label
+#from lib.figure_util import dpi, strain_color, strain_label
 
 fig = plt.figure()
-gs = gridspec.GridSpec(2, 2, width_ratios=[0.3, 0.7], height_ratios=[0.55, 0.45])
+# When with was  figure_width big then  this was perfect
+#17.73cm wide 8.87cm high
+#width, height = figure_util.get_figsize(figure_util.fig_width_big_pt, wf=1.0, hf=0.5 )
+#gs = gridspec.GridSpec(2, 2, width_ratios=[0.3, 0.7], height_ratios=[0.55, 0.45])
+
+# Making less wide 
+width, height = figure_util.cm2inch(14, 8.87)
+gs = gridspec.GridSpec(2, 2, width_ratios=[0.35, 0.65], height_ratios=[0.55, 0.45])
 
 ax_exprmt = plt.subplot(gs[0, 0])
 ax_fstrip = plt.subplot(gs[0, 1])
@@ -44,6 +51,7 @@ compiled_trace, cell_tracks = subfig_trace.load_data(comp_path, ct_path)
 compiled_trace["time"] = compiled_trace["time"]/60 # hours
 ax_pulses = subfig_trace.get_figure(ax_pulses, compiled_trace, cell_tracks)
 ax_pulses.set_ylabel("YFP/RFP ratio")
+ax_pulses.set_xlim(right=96)
 
 ################
 ## Gradient 
@@ -82,8 +90,8 @@ for a, l, ratios in zip(axes, figure_util.letters, axis_ratios):
             color="black")
 
 filename = "bf_movie_main"
-width, height = figure_util.get_figsize(figure_util.fig_width_big_pt, wf=1.0, hf=0.5 )
-fig.subplots_adjust(left=0.06, right=0.98, top=0.98, bottom=0.08, hspace=0.25, wspace=0.25)
+#width, height = figure_util.get_figsize(figure_util.fig_width_big_pt, wf=1.0, hf=0.5 )
+fig.subplots_adjust(left=0.07, right=0.99, top=0.98, bottom=0.08, hspace=0.25, wspace=0.25)
 
 fig.set_size_inches(width, height)# common.cm2inch(width, height))
 figure_util.save_figures(fig, filename, ["png", "pdf"], base_dir=this_dir )
