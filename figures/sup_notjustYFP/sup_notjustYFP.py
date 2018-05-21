9058aa526806ace17bc4345b87bc6ea8af187719
Niall Murphy <nmurphy@gmail.com>
2018-05-21 17:48:08 +0100

    look at model output

--- figures/sup_notjustYFP/sup_notjustYFP.py
+++ figures/sup_notjustYFP/sup_notjustYFP.py
@@ -5,7 +5,8 @@ from lib.resolutions import PX_TO_UM_LSM780_10x as PX_TO_UM
 import lib.figure_util
 
 this_dir = os.path.dirname(__file__)
-tilescan = os.path.join(this_dir, "../../proc_data/frozen_sigW/SigW_72hrs_center_3.tif")
+#tilescan = os.path.join(this_dir, "../../proc_data/frozen_sigw/sigw_72hrs_center_3.tif")
+tilescan = os.path.join(this_dir, "../../proc_data/frozen_sigw/sigw_48hrs_center_3.tiff")
 #"bf_pulse/proc_data/bf_live_tilescan/BF_movie15/end_20x_tilescan3
 #channels = ["0", "1"]
 # 0 is red , 1 is green
@@ -22,14 +23,15 @@ print(image.shape)
 images = [ image[1,:,:], image[0,:,:], np.zeros_like(image[0, :,:])]
 images = [ skimage.exposure.rescale_intensity(im, in_range=scales[i], out_range=(0,255))
                  for i, im in enumerate(images) ]
+images = [ i[1026:2056, 892:1700].copy() for i in images]
 #images = [ im.astype(np.uint8) for im in images] 
 #images += [ np.zeros_like(imges[-1])]
 outim = np.dstack(images)
 outim = np.rot90(outim,3)
 length = 100 
-outim = lib.figure_util.draw_scale_bar(outim, 20, 20 ,
+outim = lib.figure_util.draw_scale_bar(outim, 20, outim.shape[1]-200 ,
                                scale_length=length/PX_TO_UM,
-                               thickness=50,
-                               legend = "{0}μm".format(length), fontsize=80)
+                               thickness=30,
+                               legend = "{0}μm".format(length), fontsize=40)
 
 skimage.io.imsave(os.path.join(this_dir, "sigW.png"), outim)
