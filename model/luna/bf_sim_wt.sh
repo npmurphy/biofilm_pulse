d13f478a0a9c8c08f70ae58a8b774039f5b52d1e
Niall Murphy <nmurphy@gmail.com>
2019-11-30 12:28:21 +0000

    remove acidental stuff from sim bash file

--- model/luna/bf_sim_wt.sh
+++ model/luna/bf_sim_wt.sh
@@ -9,12 +9,6 @@ PSTR="sim_duration=6.5;initial_skip=1800.0;ascale_a=0.0;ascale_b=0.0"
 SEED=$RANDOM
 
 
-Parameters to run with [|("a0", 0.05); ("ascale_a", 0.0); ("ascale_b", 0.0); ("b0", 0.05); ("bA", 0.0);
-  ("bB", 1.0); ("dm", 0.005); ("dp", 0.005); ("initial_skip", 1800.0);
-  ("pscale_a", 0.7); ("pscale_b", 0.25); ("sim_duration", 6.5);
-  ("sim_hour", 3600.0); ("sim_minute", 60.0); ("stress", 0.0); ("trA", 0.05);
-  ("trB", 0.05); ("uA", 0.0); ("uB", 0.1)|]
-
 set -x #echo on
 mono ../lbstools/build/custom_sweeper.exe \
 	biofilm.crn \
@@ -26,15 +20,6 @@ mono ../lbstools/build/custom_sweeper.exe \
     --analysis-method bfsim_thresh \
 	--parameter-overrides "${SPORE};${SIGB};${MOD};${PSTR}"
 set +x #echo off
-# example of what final output should look like
-#custom_sweeper.exe biofilm.crn 
-#	--simulations 10000
-#	--seed 5110
-#	--sweep-file bfsim_stress.crn 
-#   --output-name bfsim_b_qp 
-#   --parameters-file carsten_map_params.crn
-#   --analysis-method bfsim_thresh 
-#   --parameter-overrides 'pscale_a=0.7;pscale_b=0.25;bA=0.0;uA=0.0;sim_duration=6.5;initial_skip=1800.0;ascale_a=0.0;ascale_b=0.0'
 
 echo "seed was ${SEED}"
   
