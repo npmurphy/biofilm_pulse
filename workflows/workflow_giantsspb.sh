254ddda526aa5f48c8a8414e66a15c6f600a1330
Niall Murphy <nmurphy@gmail.com>
2019-12-02 21:26:57 +0000

    giant sspb hasnt been run in a loooong time.

--- workflows/workflow_giantsspb.sh
+++ workflows/workflow_giantsspb.sh
@@ -2,25 +2,54 @@
 # downscale 
 #for i in `ls data/jul63xbig/*/*/*_cr_reduced20_edgemask.mat`;
 #for i in `ls data/jul63xbig/Batch3/*/*_cr_reduced20_biofilmmask.mat`;
-for i in `ls data/jul63xbig/Batch*/*/*_cr_reduced20.tiff`;
+#for i in `ls data/jul63xbig/Batch*/*/*_cr_reduced20.tiff`;
+
+# images were converted from lsm to single channel tiff files _cr (red) and _cg (green) using ImageJ. 
+
+for i in `ls data/jul63xbig/Batch*/*.lsm`;
 do  
     lsmfile=$(basename "$i");
     filename="${lsmfile%.*}";
     dirname=$(dirname "$i");
-    python giant63_svm_segmentation.py --model_file ${basedir}/svm_linear_jetseg.pkl --segment ${dirname}/${lsmfile} --extension check --patch_radius 10
-done
+
+    # Split the giant images into manageable strips 
+    python giant63_split.py -f ${dirname}/${filename}/${filename}_cr.tiff;
+    python giant63_split.py -f ${dirname}/${filename}/${filename}_cg.tiff''
+    # make map to know how the bits go together. 
+    python giant63_file_info_json_dump.py -f $i 
+    
     python giant63_mask_maker.py --downscale --file ${dirname}/${filename}/${filename}_cg.tiff 
-    #python giant63_split_cellquant.py -f ${dirname}/${filename}.lsm 
-    #python giant63_split.py -f ${dirname}/${filename}/${filename}_cr.tiff;
-    #python giant63_split.py -f ${dirname}/${filename}/${filename}_cg.tiff''
-    #python giant63_file_info_json_dump.py -f $i 
-    #python giant63_distmap.py -f $i &
-    #do python giant63_count_distances.py -f ${i} ;
-    #do python giant63_distmap.py -f $i &
-    #do python giant63_mask_maker.py --upscale -f $i &
-    #python giant63_mask_maker.py --upscale -f ${basedir}/${batch}/${filename}/${filename}_cr_reduced20_biofilmmask.mat 
-    #do python giant63_mask_maker.py --downscale --lsm_guess $i;
+    python giant63_mask_maker.py --downscale --file ${dirname}/${filename}/${filename}_cr.tiff 
+    #python giant63_mask_maker.py --downscale --lsm_guess $i;
+
+    #python giant63_svm_segmentation.py --model_file ${basedir}/svm_linear_jetseg.pkl --segment ${dirname}/${lsmfile} --extension check --patch_radius 10
+    # This SVM used eigen vectors computed for each patch in the image. 
+    # In the end the training set needed to be almost half the dataset and there was still lots of manual correction needed. 
+    # The results were compared against the standard 10x segmentation and did not really add any benifit.
+    python mask_maker.py --maskestimate -l ${dirname}/${filename}/${filename}_cr_reduced20.tiff
     #do python giant63_mask_maker.py --kmeans_model ${basedir}/kmeans_reduced_segmenter.pkl --segment -f $i;
+    # manually correct the mask
+    python mask_maker.py  --remove_cr_from_mat_path --mask_name biofilmmask --minidraw -f ${dirname}/${filename}/${filename}_cr_reduced20.tiffI
+
+    # blow it back up to full size 
+    python giant63_mask_maker.py --upscale -f ${dirname}/${filename}/${filename}_cr_reduced20_biofilmmask.mat 
+
+    # Get the distance for each sli
+    python distmap_maker.py -f $i 
+    
+    python giant63_split_spore.py -f $i
+    python giant63_split_cellquant.py -f $i
+done
+
+python giant63_data_agregate.py -db ${basedir}/file_list.tsv --data spores -f ${basedir}/${batch}/${filename}.lsm 
+python giant63_data_agregate.py -db ${basedir}/file_list.tsv --data cells -f ${basedir}/${batch}/${filename}.lsm 
+python giant63_total_agregate.py -o ${basedir}/autocor_data.h5 --data spores -f ${basedir}/Batch*/*.h5 
+python giant63_total_agregate.py -o ${basedir}/autocor_data.h5 --data cells -f ${basedir}/Batch*/*.h5
+
+
+
+
+
 batch="Batch1"
 filename="JLB077_48hrs_center_2_1" 
 #python giant63_split_spore.py -f ${basedir}/${batch}/${filename}.lsm 
@@ -97,7 +126,7 @@ python giant63_mask_maker.py  --mask_name edgemask --minidraw -f ${basedir}/${ba
 python giant63_mask_maker.py --upscale -f ${basedir}/${batch}/${filename}/${filename}_cr_reduced20_edgemask.mat 
 python giant63_mask_maker.py --upscale -f ${basedir}/${batch}/${filename}/${filename}_cr_reduced20_biofilmmask.mat 
 python giant63_distmap.py -f ${basedir}/${batch}/${filename}.lsm 
-python giant63_count_distances.py -f ${basedir}/${batch}/${filename}.lsm 
+#python giant63_count_distances.py -f ${basedir}/${batch}/${filename}.lsm 
 python giant63_split_spore.py -f ${basedir}/${batch}/${filename}.lsm 
 #python giant63_split_cell.py -f ${basedir}/${batch}/${filename}.lsm 
 python giant63_split_cellquant.py -f ${basedir}/${batch}/${filename}.lsm 
